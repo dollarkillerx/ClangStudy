4163013d69cda2f1de94ac6fbf4dd7ab0ad0569d
dollarkillerx <adapawang@gmail.com>
2019-12-02 13:39:35 +0800

    in

---
+++ cmake-build-debug/CMakeFiles/CMAKE_C_STANDARD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMAKE_C_STANDARD"
+  "CMAKE_C_STANDARD.pdb"
+  "CMakeFiles/CMAKE_C_STANDARD.dir/day1/app5.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMAKE_C_STANDARD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMAKE_C_STANDARD.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CMAKE_C_STANDARD.
+# This may be replaced when dependencies are built.
